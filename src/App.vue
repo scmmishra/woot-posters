4da6de94506d8dd0ca9edf00edee928dd5c6073c
Shivam Mishra <scm.mymail@gmail.com>
2023-03-24 12:02:23 +0530

    feat: add input

--- src/App.vue
+++ src/App.vue
@@ -112,12 +112,15 @@ function setImage() {
         :background="selectedMesh"
       />
       <div class="flex justify-between w-full">
-        <input
-          type="file"
-          id="imageInput"
-          @change="setImage"
-          accept="image/*"
-        />
+        <div>
+          <input
+            class="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-gray-300 bg-clip-padding py-[0.32rem] px-3 text-base font-normal text-black transition duration-300 ease-in-out file:-mx-3 file:-my-[0.32rem] file:overflow-hidden file:rounded-none file:border-0 file:border-solid file:border-inherit file:bg-gray-100 file:px-3 file:py-[0.32rem] file:text-black-700 file:transition file:duration-150 file:ease-in-out file:[margin-inline-end:0.75rem] file:[border-inline-end-width:1px] hover:file:bg-gray-200 focus:border-primary focus:text-gray-900 focus:shadow-[0_0_0_1px] focus:shadow-primary focus:outline-none dark:border-gray-600 dark:text-gray-700 dark:file:bg-gray-700 dark:file:text-gray-100"
+            type="file"
+            @change="setImage"
+            id="imageInput"
+            accept="image/*"
+          />
+        </div>
         <DownloadButton />
       </div>
     </section>
